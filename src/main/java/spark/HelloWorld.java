2e95b28c0dbd12c182d050ac890602e657850b25
Ziyanda <ziyandandlamafa47@gmail.com>
2019-12-06 15:01:28 +0200

    initial commit

---
+++ src/main/java/spark/HelloWorld.java
@@ -0,0 +1,32 @@
+package spark;
+
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> {
+            return new ModelAndView(new HashMap<>(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/hello", (req, res) -> {
+
+            Map<String, String> dataMap = new HashMap<>();
+            dataMap.put("name", "Ziyanda");
+            return new ModelAndView(dataMap, "hello.hbs");
+
+        }, new HandlebarsTemplateEngine());
+    }
+
+}
